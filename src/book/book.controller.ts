f525d69797904bb542d1d9a00ab16da3ce0b1f8e
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-03-20 20:38:52 -0300

    feat: implemented controller

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -9,30 +9,27 @@ export class BookController {
   }
 
   async create(req: Request, res: Response) {
-    console.log('CREATE');
-
-    res.status(201).json();
+    const book = await this.bookService.create(req.body);
+    res.status(201).json(book);
   }
 
   async findAll(req: Request, res: Response) {
-    this.bookService.findAll();
-    console.log('findAll');
-
-    res.status(200).json();
+    const books = await this.bookService.findAll();
+    res.status(200).json(books);
   }
 
   async findOne(req: Request, res: Response) {
-    console.log('findOne');
-    res.status(200).json();
+    const book = await this.bookService.findOne(req.params.id);
+    res.status(200).json(book);
   }
 
   async update(req: Request, res: Response) {
-    console.log('update');
-    res.status(200).json();
+    const updatedBook = this.bookService.update(req.params.id, req.body);
+    res.status(200).json(updatedBook);
   }
 
   async delete(req: Request, res: Response) {
-    console.log('delete');
+    await this.bookService.delete(req.params.id);
     res.status(204);
   }
 }
